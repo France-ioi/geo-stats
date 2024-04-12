6c0db3d21ec6083b007f1194525064b9fa2b2b1f
Michel Blockelet <blockelet@crans.org>
2024-04-12 20:00:14 +0000

    More robust database connection

--- index.js
+++ index.js
@@ -16,24 +16,31 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+const pendingQueries = [];
+let connecting = false;
+
 function mysqlConnect() {
+    if(connecting) {
+        return;
+    }
+    connecting = true;
     connection.connect((err) => {
+        connecting = false;
         if (err) {
             console.error('Error connecting to database:', err);
-            setTimeout(mysqlConnect, 2000);
+            setTimeout(() => mysqlConnect(callback), 2000);
             return;
         }
         console.log('Connected to database!');
+        while(pq = pendingQueries.shift()) {
+            sqlQuery(pq.query, pq.params, pq.callback);
+        }
     });
 }
 
 connection.on('error', (err) => {
-    console.error('Error connecting to database:', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        mysqlConnect();
-    } else {
-        throw err;
-    }
+    console.error('Error on database connection:', err);
+    setTimeout(mysqlConnect, 2000);
 });
 
 mysqlConnect();
@@ -55,8 +62,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('geo-stats is active.');
 });
+
+function sqlQuery(query, params, callback) {
+    // if connection is down, try to reconnect
+    if (connection.state === 'disconnected') {
+        pendingQueries.push({ query, params, callback });
+        return mysqlConnect();
+    }
+    connection.query(query, params, (err, rows) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            callback(err);
+            return;
+        }
+        callback(null, rows);
+    });
+}
 
 function getCityId(ip, success, error) {
     const cityId = cityCache.get(ip);
@@ -70,10 +93,10 @@ function getCityId(ip, success, error) {
     const subdivisions = info.subdivisions && info.subdivisions.map(subdivision => getName(subdivision.names)).join(', ') || '';
     const countryCode = info.country.iso_code;
     // check in database if city exists from country_code, subdivisions and city
-    connection.query('SELECT id FROM cities WHERE country_code = ? AND subdivisions = ? AND city = ?', [countryCode, subdivisions, city], (err, rows) => {
+    sqlQuery('SELECT id FROM cities WHERE country_code = ? AND subdivisions = ? AND city = ?', [countryCode, subdivisions, city], (err, rows) => {
         if (rows.length === 0) {
             // insert city in database
-            connection.query('INSERT INTO cities (country_code, subdivisions, city, longitude, latitude) VALUES (?, ?, ?, ?, ?)', [countryCode, subdivisions, city, info.location.longitude, info.location.latitude], (err, result) => {
+            sqlQuery('INSERT INTO cities (country_code, subdivisions, city, longitude, latitude) VALUES (?, ?, ?, ?, ?)', [countryCode, subdivisions, city, info.location.longitude, info.location.latitude], (err, result) => {
                 if (err) {
                     console.error('Error inserting city in database:', err);
                     error('Error inserting city in database.');
@@ -91,7 +114,7 @@ function getCityId(ip, success, error) {
 
 function saveData(cityId, platformId, user, data, success, error) {
     // save data in database
-    connection.query('INSERT INTO records (datetime, platform_id, city_id, user, data) VALUES (NOW(), ?, ?, ?, ?)', [platformId, cityId, user, data], (err, result) => {
+    sqlQuery('INSERT INTO records (datetime, platform_id, city_id, user, data) VALUES (NOW(), ?, ?, ?, ?)', [platformId, cityId, user, data], (err, result) => {
         if (err) {
             console.error('Error inserting data in database:', err);
             error('Error inserting data in database.');
@@ -102,7 +125,7 @@ function saveData(cityId, platformId, user, data, success, error) {
 }
 
 // Load platforms from database, indexed by key
-connection.query('SELECT * FROM platforms', (err, rows) => {
+sqlQuery('SELECT * FROM platforms', (err, rows) => {
     if (err) {
         console.error('Error loading platforms from database:', err);
         return;
